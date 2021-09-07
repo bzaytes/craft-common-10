ea0e42004be6dc65be920b787254b7e3196ed4a8
craftgithubuser <craftknowlge.soresa@gmail.com>
2021-09-06 23:04:15 -0400

    TCL file commit

--- Performing TCL.sql
+++ Performing TCL.sql
@@ -16,6 +16,22 @@ primary key(Cust_ID)
 truncate customers;
 
 start transaction;
+insert into customers
+values(111, 'Nick', 'Jones', '26', 'Male', 'USA', '20000000');
+insert into customers
+values(222, 'Antony', 'Martial', '24', 'Male', 'France', '10000000');
+insert into customers
+values(333, 'Nebiha', 'Amir', '24', 'Femal', 'uzbekistan', '36000000');
+insert into customers
+values(444, 'Soresa', 'Hailu', '24', 'Male', 'Unkown', '34000000');
+insert into customers
+values(555, 'Beka', 'Haile', '25', 'Male', 'kazakhstan', '400000');
+insert into customers
+values(777, 'Poul', 'Pogba', '26', 'Male', 'France', '20000000');
+insert into customers
+values(888, 'Betty', 'G', '27', 'Femal', 'Ethiopia', '70000000');
+insert into customers
+values(999, 'Nick', 'Jones', '26', 'Male', 'USA', '20000000');
 
 
 savepoint ins;
